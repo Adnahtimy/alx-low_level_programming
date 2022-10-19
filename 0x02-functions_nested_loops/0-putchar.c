3994c8c4ed2666077ee75aeee0c0e65b5defa60b
adnahtimy <sheyidaretimilehin@gmail.com>
2022-10-18 21:22:43 -0700

    More tasks to solve at ALX

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+char *sh = "Main";
+while (*sh)
+{
+_putchar(*sh);
+sh++;
+}
+_putchar(n);
+return (0);
+}
