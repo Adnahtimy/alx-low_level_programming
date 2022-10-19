3b48f4ecba2075fadce333a1a66811e14e9d8156
adnahtimy <sheyidaretimilehin@gmail.com>
2022-10-18 22:52:48 -0700

    More tasks to solve at ALX

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <1-alphabet.h>
+/**
+ * print_alphabet - Entry point
+ * Return: Always 0
+ */
+int print_alphabet(void)
+{
+	int x
+	for (x = a; x <= z; x++);
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
