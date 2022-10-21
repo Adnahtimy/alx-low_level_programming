c4f621cadc339147a59bf41c8d69d2112a2b995a
adnahtimy <sheyidaretimilehin@gmail.com>
2022-10-20 17:55:23 -0700

    More tasks to solve at ALX

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#include <unistd.h>
-/** 
+#include <unistd.h> "main.h"
+/**
  * main - Entry point
- * Return: Always 1
+ * Return : Always 1
  */
 int main(void)
 {
-        write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-        return (1);
+	printf(1, "and that piece of art is useful Dora Korpar, 2015-10-19\n", 59);
+	return 1;
 }
